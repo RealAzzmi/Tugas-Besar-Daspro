3b8cfe12b4900b9cbff127758fffe4864a351a08
hasnafaa <hasnafa04@gmail.com>
2023-04-20 20:25:35 +0700

    typo

--- functions/f06_bangun.py
+++ functions/f06_bangun.py
@@ -12,7 +12,7 @@ def run():
         sand_candi = random.randint(MIN_RAND, MAX_RAND)
         stone_candi = random.randint(MIN_RAND, MAX_RAND)
         water_candi = random.randint(MIN_RAND, MAX_RAND)
-        print(f"Bahan bangunanan yang dimilki: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
+        print(f"Bahan bangunan yang dimiliki: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
         print(f"Bahan bangunan yang dibutuhkan untuk membuat 1 candi: (pasir: {sand_candi}, batu: {stone_candi}, air: {water_candi}).")
         if not(sand_candi <= database.bahan_bangunan.bahan_list.sand and stone_candi <= database.bahan_bangunan.bahan_list.stone and water_candi <= database.bahan_bangunan.bahan_list.water):
             print("Bahan bangunan tidak mencukupi. Candi tidak bisa dibangun!")
@@ -22,12 +22,12 @@ def run():
         database.bahan_bangunan.bahan_list.water -= water_candi
         if database.candi.candi_list.size == MAX_CANDI:
             print(f"Jumlah candi sudah mencapai nilai maksimum ({MAX_CANDI}). Candi akan dibangun dan bahan bangunan akan berkurang, namun candi tidak akan tercatat.")
-            print(f"Sisa bahan bangunanan yang dimilki: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
+            print(f"Sisa bahan bangunan yang dimiliki: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
             print(f"Sisa candi yang perlu dibangun: 0.")
             return
         database.candi.add_candi(database.candi.smallest_unavailable_id(), variables.username, sand_candi, stone_candi, water_candi)
         print("Candi berhasil dibangun.")
-        print(f"Sisa bahan bangunanan yang dimilki: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
+        print(f"Sisa bahan bangunan yang dimiliki: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
         print(f"Sisa candi yang perlu dibangun: {MAX_CANDI - database.candi.candi_list.size}.")
 
 
--- functions/f07_kumpul.py
+++ functions/f07_kumpul.py
@@ -12,9 +12,9 @@ def run():
         sand_candi = random.randint(MIN_RAND, MAX_RAND)
         stone_candi = random.randint(MIN_RAND, MAX_RAND)
         water_candi = random.randint(MIN_RAND, MAX_RAND)
-        print(f"Bahan bangunanan yang dimilki sebelum kumpul bahan: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
+        print(f"Bahan bangunan yang dimiliki sebelum kumpul bahan: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
         print(f"Bahan bangunan yang didapatkan: (pasir: {sand_candi}, batu: {stone_candi}, air: {water_candi}).")
         database.bahan_bangunan.bahan_list.sand += sand_candi
         database.bahan_bangunan.bahan_list.stone += stone_candi
         database.bahan_bangunan.bahan_list.water += water_candi
-        print(f"Bahan bangunanan yang dimilki setelah kumpul bahan: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")
+        print(f"Bahan bangunan yang dimiliki setelah kumpul bahan: (pasir: {database.bahan_bangunan.bahan_list.sand}, batu: {database.bahan_bangunan.bahan_list.stone}, air: {database.bahan_bangunan.bahan_list.water}).")

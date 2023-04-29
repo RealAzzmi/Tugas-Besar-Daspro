d45170ec51a778f5c0059ca0c55f8479c636cb12
Harryts1 <harrytrumansuhalim8@gmail.com>
2023-04-29 23:28:19 +0700

    command

--- functions/f13_load.py
+++ functions/f13_load.py
@@ -3,19 +3,21 @@ from argparse import *
 import os
 
 def run():
+    # Melakukan argument processing
     parser = ArgumentParser()
     parser.add_argument("nama_folder", nargs='?', type=str, default='')
     arg = parser.parse_args()
     folder = arg.nama_folder
-    if os.path.isdir(folder) :
+    # Mengecek apakah terdapat folder tersebut
+    if os.path.isdir(folder) : # Output jika folder terdeteksi
         print("Loading...")
         print("\nSelamat datang di program \"Manajerial Candi\"")
         print("Masukkan command \"help\" untuk daftar command yang dapat kamu panggil.")
         variables.load_folder = folder
-    elif folder == '':
+    elif folder == '': # Output jika user tidak menginput nama folder
         print("\nTidak ada nama folder yang diberikan!\n")
         print("Usage: " + "python main.py" + " <nama_folder>")
         exit()
-    else :
+    else : # Output jika folder tidak terdeteksi
         print(f"\nfolder \"{folder}\" tidak ditemukan")
         exit()
--- functions/f14_save.py
+++ functions/f14_save.py
@@ -4,16 +4,19 @@ from constants import PARENT_FOLDER
 from algorithms.string_processing import split, count_letters
 
 def run():
+    # Memasukkan nama folder 
     folder_name = input("\nMasukkan nama folder (Gunakan file seperator yang sesuai; \\ untuk Windows dan / untuk macOS dan Linux): ")
+    # Melakukan save terhadap folder tersebut
     folder_location = os.path.relpath(os.path.join(PARENT_FOLDER, folder_name))
     print("\nSaving...\n")
     folder_components = split(folder_location, os.sep)
     temp = ''
     for i in range(count_letters(folder_location, os.sep) + 1):
         temp += folder_components[i] + os.sep
-        if os.path.isdir(temp):
+        # Mengecek apakah folder tersebut sudah ada atau tidak
+        if os.path.isdir(temp): # Jika sudah ada
             continue
-        else:
+        else: # Jika belum ada
             os.makedirs(temp)
             print("Membuat folder", temp)
 

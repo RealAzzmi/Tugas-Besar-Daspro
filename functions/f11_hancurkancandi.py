8921a4dc57ad39b03d9e710e70e1028726f1aa01
AbiyyuR <abiyyuraihan18@gmail.com>
2023-04-30 17:38:34 +0700

    Update f11_hancurkancandi.py

--- functions/f11_hancurkancandi.py
+++ functions/f11_hancurkancandi.py
@@ -2,16 +2,20 @@ import database.user, database.bahan_bangunan, database.candi
 import variables
 
 def run():
+    # Mengecek apakah user sudah login atau belum
     if variables.login == False:
         print("Anda belum login.")
+    # Mengeceh apakah role user merupakan roro jonggrang    
     elif variables.role != "roro_jonggrang" :
         print("Anda tidak mempunyai akses; hanya Roro Jonggrang yang bisa \"menghancurkan candi\".")
+    # Mengecek apakah candi dengan ID tersebut ada atau tidak
     else:
         id_candi = int(input("Masukkan ID candi: "))
         is_exist, candi = database.candi.is_candi_exist(id_candi)
         if not is_exist:
             print("Tidak ada candi dengan ID tersebut.")
             return
+        # Mengecek apakah user ingin menghancurkan candi atau tidak
         confirmation = input(f"Apakah anda yakin ingin menghancurkan candi ID: {id_candi} (Y/N)? ")
         while not(confirmation == 'n' or confirmation == 'N' or confirmation == 'y' or confirmation == 'Y'):
             confirmation = input(f"Apakah anda yakin ingin menghancurkan candi ID: {id_candi} (Y/y/N/n)? ")

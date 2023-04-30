f3e59f989180577783bd0fcb59a59b07236cf6c4
hasnafaa <76027195+hasnafaa@users.noreply.github.com>
2023-04-30 21:21:56 +0700

    tambah command

--- functions/f12_ayamberkokok.py
+++ functions/f12_ayamberkokok.py
@@ -4,11 +4,14 @@ from constants import MAX_CANDI
 import sys
 
 def run():
+    # Mengecek apakah sudah login
     if variables.login == False:
         print("Anda belum login")
+    # Mengecek apakah role-nya Roro Jonggrang
     elif variables.role != "roro_jonggrang":
         print("Anda tidak mempunyai akses; hanya Roro Jonggrang yang memiliki kemampuan ini.")
     else:
+        # Jika kondisi jumlah candi kurang dari 100 (jumlah maksimal candi)
         if database.candi.candi_list.size < MAX_CANDI:
             print(f"""Kukuruyuk.. Kukuruyuk..
 
@@ -18,10 +21,10 @@ Selamat, Roro Jonggrang memenangkan permainan!
 
 *Bandung Bondowoso angry noise*
 Roro Jonggrang dikutuk menjadi candi.""")
-        else: # database.candi.candi_list.size >= MAX_CANDI
+        else: # database.candi.candi_list.size >= MAX_CANDI (jumlah candi lebih dari atau sama dengan 100)
             print(f"""Kukuruyuk.. Kukuruyuk..
 
 Jumlah Candi: {database.candi.candi_list.size}
 
 Yah, Bandung Bondowoso memenangkan permainan!""")
-        sys.exit()
+        sys.exit() # Otomatis keluar dari program

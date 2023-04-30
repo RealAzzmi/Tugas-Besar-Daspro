076f2c3032663f3f192793aa1251b5412535286d
hasnafaa <76027195+hasnafaa@users.noreply.github.com>
2023-04-30 21:25:45 +0700

    tambah command

--- functions/f16_exit.py
+++ functions/f16_exit.py
@@ -2,14 +2,14 @@ import functions.f14_save
 import sys
 
 def run():
-    save_file = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    check = False
+    save_file = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ") # Meminta masukan konfirmasi save file
+    check = False # Set kondisi awal
     while check == False:
-        if save_file == "y" or save_file == "Y":
-            functions.f14_save.run()
+        if save_file == "y" or save_file == "Y": # Jika kondisi user setuju
+            functions.f14_save.run() # Melakukan program F14-Save
             check = True
-        elif save_file == "n" or save_file == "N":
+        elif save_file == "n" or save_file == "N": # Jika kondisi user tidak setuju
             check = True
         else:
-            save_file = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    sys.exit()
+            save_file = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ") # Menanyakan ulang hingga masukan menjadi valid
+    sys.exit() # Keluar dari program
